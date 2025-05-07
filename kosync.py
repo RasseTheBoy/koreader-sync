7a2128abea1d61794643d9920480ace63d72eae9
RasseTheBoy <rassemichale@gmail.com>
2025-05-07 13:51:16 +0300

    Making the code more pythonic
    Added:
    - kosync.py
      - docstrings to all functions (Sphinx style)
      - return type-hint to all functions
      - comments within functions
    - .gitignore
      - `.vscode/`
        - directory that is generated by VSCode
    
    Modified:
    - kosync.py
      - function body texts rearanged to be more readable
      - removed and moved some unnecessary variable declarations
      - comments within functions
      - the `time` and `uuid` imports are now using `from ... import ...`
    - docker-compose.yaml
      - builds the docker image from the local file, instead of fetching it from DockerHub

--- kosync.py
+++ kosync.py
@@ -1,26 +1,26 @@
 # -*- coding: utf-8 -*-
-import time
-import uuid
 from distutils.util import strtobool
-from os import getenv
-from typing import Optional
-
+from pydantic import BaseModel
 from dotenv import load_dotenv
+from tinydb import Query, TinyDB
+from typing import Optional
+from uuid import uuid1
+from time import time
+from os import getenv
 from fastapi import FastAPI, Header
 from fastapi.responses import JSONResponse
-from pydantic import BaseModel
-from tinydb import Query, TinyDB
+
 
 app = FastAPI(openapi_url=None, redoc_url=None)
-db = TinyDB("data/db.json")
-users = db.table("users")
-documents = db.table("documents")
+db = TinyDB('data/db.json')
+users = db.table('users')
+documents = db.table('documents')
 load_dotenv()
+
 
 class KosyncUser(BaseModel):
     username: Optional[str] = None
     password: Optional[str] = None
-
 
 class KosyncDocument(BaseModel):
     document: Optional[str] = None
@@ -30,110 +30,193 @@ class KosyncDocument(BaseModel):
     device_id: Optional[str] = None
 
 
-@app.post("/users/create")
-def register(kosync_user: KosyncUser):
-	# Check whether new registrations are allowed on this server based on the OPEN_REGISTRATIONS environment variable.
-	# By default registrations are enabled.
-	registrations_allowed = bool(strtobool(getenv("OPEN_REGISTRATIONS", "True")))
-	if registrations_allowed:
-		# check if username or password is missing
-		if kosync_user.username is None or kosync_user.password is None:
-			return JSONResponse(status_code=400, content={"message": f"Invalid request"})
-		# check if user already exists
-		QUser = Query()
-		if users.contains(QUser.username == kosync_user.username):
-			return JSONResponse(status_code=409, content="Username is already registered.")
-		# register new user
-		if users.insert({'username': kosync_user.username, 'password': kosync_user.password}):
-			return JSONResponse(status_code=201, content={"username": kosync_user.username})
-		# if something went wrong
-		return JSONResponse(status_code=500, content="Unknown server error")
-	else:
-		return JSONResponse(status_code=403, content="This server is currently not accepting new registrations.")
+@app.post('/users/create')
+def register(kosync_user: KosyncUser) -> JSONResponse:
+    """Register a new user (if OPEN_REGISTRATIONS is set to True).
+    
+    :param KosyncUser kosync_user: The user to be registered.
+    
+    :return: JSONResponse
+    """
+    # Registrerations are not allowed on this server
+    if not strtobool(getenv('OPEN_REGISTRATIONS', 'True')):
+        return JSONResponse(status_code=403, content='This server is currently not accepting new registrations.')
 
-
-@app.get("/users/auth")
-def authorize(x_auth_user: Optional[str] = Header(None), x_auth_key: Optional[str] = Header(None)):
-    # check if username or password is missing
-    if x_auth_user is None or x_auth_key is None:
-        return JSONResponse(status_code=401, content={"message": f"Unauthorized"})
-    # check if username is in database
+    # Check if username or password is missing
+    elif None in (kosync_user.username, kosync_user.password):
+        return JSONResponse(status_code=400, content={'message': 'Invalid request'})
+    
+    # Check if user already exists
     QUser = Query()
-    # check username and password combination
-    if users.contains(QUser.username == x_auth_user):
-        if users.contains((QUser.username == x_auth_user) & (QUser.password == x_auth_key)):
-            return JSONResponse(status_code=200, content={"authorized": f"OK"})
-        else:
-            return JSONResponse(status_code=401, content={"message": f"Unauthorized"})
-    return JSONResponse(status_code=403, content={"message": f"Forbidden"})
+    
+    if users.contains(QUser.username == kosync_user.username):
+        return JSONResponse(status_code=409, content='Username is already registered.')
+
+    # Register new user
+    elif users.insert({'username': kosync_user.username, 'password': kosync_user.password}):
+        return JSONResponse(status_code=201, content={'username': kosync_user.username})
+    
+    # Something went wrong
+    return JSONResponse(status_code=500, content='Unknown server error')
 
 
-@app.put("/syncs/progress")
-def update_progress(kosync_document: KosyncDocument, x_auth_user: Optional[str] = Header(None),
-                    x_auth_key: Optional[str] = Header(None)):
-    # check if username or password is missing
-    if x_auth_user is None or x_auth_key is None:
-        return JSONResponse(status_code=401, content={"message": f"Unauthorized"})
+@app.get('/users/auth')
+def authorize(
+    x_auth_user: Optional[str] = Header(None),
+    x_auth_key: Optional[str] = Header(None)
+) -> JSONResponse:
+    """Check if username and password combination is valid.
+    
+    :param str x_auth_user: The username.
+    :param str x_auth_key:
+
+    :return: JSONResponse
+    """
+    # Check if username or password is missing
+    if not (x_auth_user and x_auth_key):
+        return JSONResponse(status_code=401, content={'message': 'Unauthorized'})
+    
+    QUser = Query()
+    
+    # Check if username is in database
+    if not users.contains(QUser.username == x_auth_user):
+        return JSONResponse(status_code=403, content={'message': 'Forbidden'})
+    
+    # Check username and password combination
+    elif not users.contains((QUser.username == x_auth_user) & (QUser.password == x_auth_key)):
+        return JSONResponse(status_code=401, content={'message': 'Unauthorized'})
+    
+    # Authentication successful
+    return JSONResponse(status_code=200, content={'authorized': 'OK'})
+
+
+@app.put('/syncs/progress')
+def update_progress(
+    kosync_document: KosyncDocument,
+    x_auth_user: Optional[str] = Header(None),
+    x_auth_key: Optional[str] = Header(None)
+) -> JSONResponse:
+    """Update progress of a document.
+
+    :param KosyncDocument kosync_document: The document to be updated.
+    :param str x_auth_user: The username.
+    :param str x_auth_key: The password.
+    
+    :return: JSONResponse
+    """
+    # Check if username or password is missing
+    if not (x_auth_user and x_auth_key):
+        return JSONResponse(status_code=401, content={'message': 'Unauthorized'})
+    
     QUser = Query()
     QDocument = Query()
-    # check if username is in database
+    
+    # Check if username is in database
     if not users.contains(QUser.username == x_auth_user):
-        return JSONResponse(status_code=403, content={"message": f"Forbidden"})
-    # check username and password combination before put data in database
-    if users.contains((QUser.username == x_auth_user) & (QUser.password == x_auth_key)):
-        # add new document progress
-        timestamp = int(time.time())
-        if kosync_document.document is None or kosync_document.progress is None or kosync_document.percentage is None \
-                or kosync_document.device is None or kosync_document.device_id is None:
-            return JSONResponse(status_code=500, content="Unknown server error")
-        else:
-            if documents.upsert({'username': x_auth_user, 'document': kosync_document.document,
-                                 'progress': kosync_document.progress, 'percentage': kosync_document.percentage,
-                                 'device': kosync_document.device, 'device_id': kosync_document.device_id,
-                                 'timestamp': timestamp}, (QDocument.username == x_auth_user) &
-                                                          (QDocument.document == kosync_document.document)):
-                return JSONResponse(status_code=200,
-                                    content={"document": kosync_document.document, "timestamp": timestamp})
-    else:
-        return JSONResponse(status_code=401, content={"message": f"Unauthorized"})
+        return JSONResponse(status_code=403, content={'message': 'Forbidden'})
+    
+    # Check username and password combination
+    elif not users.contains((QUser.username == x_auth_user) & (QUser.password == x_auth_key)):
+        return JSONResponse(status_code=401, content={'message': 'Unauthorized'})
+    
+    # Check if document is in database
+    elif None in (
+        kosync_document.document,
+        kosync_document.progress,
+        kosync_document.percentage,
+        kosync_document.device,
+        kosync_document.device_id
+    ):
+        return JSONResponse(status_code=500, content='Unknown server error')
+    
+    # Update document in database
+    timestamp = int(time())
+    if documents.upsert(
+        {
+            'username': x_auth_user,
+            'document': kosync_document.document,
+            'progress': kosync_document.progress,
+            'percentage': kosync_document.percentage,
+            'device': kosync_document.device,
+            'device_id': kosync_document.device_id,
+            'timestamp': timestamp
+        },
+        (QDocument.username == x_auth_user) & (QDocument.document == kosync_document.document)
+    ):
+        return JSONResponse(
+            status_code=200,
+            content={'document': kosync_document.document, 'timestamp': timestamp}
+        )
+        
+    # Something went wrong
+    return JSONResponse(status_code=500, content='Unknown server error')
 
 
-@app.get("/syncs/progress/{document}")
-def get_progress(document: Optional[str] = None, x_auth_user: Optional[str] = Header(None),
-                 x_auth_key: Optional[str] = Header(None)):
-    # check if username or password is missing
-    if x_auth_user is None or x_auth_key is None:
-        return JSONResponse(status_code=401, content={"message": f"Unauthorized"})
-    # check if document parameter exists
-    if document is None:
-        return JSONResponse(status_code=500, content="Unknown server error")
+@app.get('/syncs/progress/{document}')
+def get_progress(
+    document: Optional[str] = None,
+    x_auth_user: Optional[str] = Header(None),
+    x_auth_key: Optional[str] = Header(None)
+) -> JSONResponse:
+    """Get the progress of a document.
+    
+    :param str document: The document to get the progress of.
+    :param str x_auth_user: The username.
+    :param str x_auth_key: The password.
+    
+    :return: JSONResponse
+    """
+    # Check if username or password is missing
+    if not (x_auth_user and x_auth_key):
+        return JSONResponse(status_code=401, content={'message': 'Unauthorized'})
+    
+    # Check if document parameter exists
+    elif document is None:
+        return JSONResponse(status_code=500, content='Unknown server error')
 
     QUser = Query()
     QDocument = Query()
 
-    # check if username is in database
+    # Check if username is in database
     if not users.contains(QUser.username == x_auth_user):
-        return JSONResponse(status_code=403, content={"message": f"Forbidden"})
+        return JSONResponse(status_code=403, content={'message': 'Forbidden'})
 
-    # check username and password combination before get progress data
-    if users.contains((QUser.username == x_auth_user) & (QUser.password == x_auth_key)):
-        # get document progress if user has the document
-        result = documents.get((QDocument.username == x_auth_user) & (QDocument.document == document))
-        if result:
-            rrdi = bool(strtobool(getenv("RECEIVE_RANDOM_DEVICE_ID", "False")))
-            if rrdi == False:
-                device_id = result["device_id"]
-            else:
-                device_id = uuid.uuid1()
-                device_id = str(device_id.hex).upper()
-            return JSONResponse(status_code=200,
-                                content={'username': x_auth_user, 'document': result["document"],
-                                         'progress': result["progress"], 'percentage': result["percentage"],
-                                         'device': result["device"], 'device_id': device_id,
-                                         'timestamp': result["timestamp"]})
-    else:
-        return JSONResponse(status_code=401, content={"message": f"Unauthorized"})
+    # Check username and password combination
+    elif not users.contains((QUser.username == x_auth_user) & (QUser.password == x_auth_key)):
+        return JSONResponse(status_code=401, content={'message': 'Unauthorized'})
+    
+    # Get document progress if user has the document
+    result = documents.search((QDocument.username == x_auth_user) & (QDocument.document == document))
+    
+    # Document not found
+    if not result:
+        return JSONResponse(status_code=404, content={'message': 'Not found'})
+    
+    # Select the first result
+    result = result[0]
+    
+    # Get device ID from either database or generate one; depends on the 'RECEIVE_RANDOM_DEVICE_ID' environment variable
+    device_id = str(uuid1().hex).upper() if strtobool(getenv('RECEIVE_RANDOM_DEVICE_ID', 'False')) else result['device_id']
+     
+    # Return document progress
+    return JSONResponse(
+        status_code=200,
+        content={
+            'username': x_auth_user,
+            'document': result['document'],
+            'progress': result['progress'],
+            'percentage': result['percentage'],
+            'device': result['device'],
+            'device_id': device_id,
+            'timestamp': result['timestamp']
+        }
+    )
+    
+    
+@app.get('/healthstatus')
+def get_healthstatus() -> JSONResponse:
+    """Get the health status of the server.
 
-@app.get("/healthstatus")
-def get_healthstatus():
-    return JSONResponse(status_code=200, content={"message": f"healthy"})
+    :return: JSONResponse
+    """
+    return JSONResponse(status_code=200, content={'message': 'healthy'})
